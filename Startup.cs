d1462f5bb7c7c286b5598f49efcb0b5ea364f4e6
unknown <aymansafi1@hotmail.com>
2018-07-07 14:01:29 +0300

    add angular core

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,7 @@ namespace DatingApp.api
             services.AddDbContext<DataContext>(x =>
              x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +38,7 @@ namespace DatingApp.api
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
         }
     }
